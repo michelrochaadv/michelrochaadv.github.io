7e349558bd7d3a91571127bc688fdf257dea36ca
michelrochaadv <michelrochasoares@gmail.com>
2026-07-26 10:23:37 -0300

    Lote 26/07/2026: update_lote.py

--- update_lote.py
+++ update_lote.py
@@ -2,9 +2,9 @@
 """Atualiza páginas de categoria (todos os artigos) e index.html (Publicados Hoje + carrosséis)."""
 import json, re
 
-DATE = "2026-07-25"
-DATE_DISPLAY = "25/07/2026"
-DATE_SHORT = "25 jul 2026"
+DATE = "2026-07-26"
+DATE_DISPLAY = "26/07/2026"
+DATE_SHORT = "26 jul 2026"
 
 AREAS = {
     "penal": "Penal",
